8b1d20b7743a24e5097e7d281044a23e9c9917f1
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2013-02-19 18:38:59 -0800

    wip

---
+++ 2013/go-stdlib-part3/reflect_new.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+
+	type SomeStruct struct {
+		aa [10]int
+		bb [20]int
+	}
+
+	var intPtr SomeStruct
+	intPtr2 := reflect.New(reflect.TypeOf(intPtr))
+
+	fmt.Printf("New (Pointer to T): %v: Type %v\n", intPtr2.Elem().Interface(), intPtr2.Type())
+
+	intPtr3 := reflect.Zero(reflect.TypeOf(intPtr))
+
+	fmt.Printf("Zero (T): %v: Type:%v\n", intPtr3.Interface(), intPtr3.Type())
+}
