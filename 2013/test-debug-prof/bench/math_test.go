62a8d6d5bd7d3cc49f52f83f5b89a2179809dca0
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2013-04-22 21:14:55 -0700

    format and fixes

--- 2013/test-debug-prof/bench/math_test.go
+++ 2013/test-debug-prof/bench/math_test.go
@@ -2,8 +2,11 @@ package math
 
 import "testing"
 
-func BenchmarkDiv(b *testing.B) {
-	for i := 0; i < b.N; i++ { // go test will increase N to get good timing
+// START1 OMIT
+func BenchmarkDiv(b *testing.B) { // HL
+	for i := 0; i < b.N; i++ { // go test will increase N to get good timing 
 		Div(10.0, 3.0)
 	}
 }
+// END1 OMIT
+
