541cbaf863a6ab63f3789d4fb2156afedd7e3b14
Buddy Sandidge <buddy.sandidge@convertro.com>
2013-08-01 00:36:47 -0700

    Remove unused port

--- go/req-client/main.go
+++ go/req-client/main.go
@@ -7,7 +7,6 @@ func main() {
 	context, _ := zmq.NewContext()
 	socket, _ := context.NewSocket(zmq.REQ)
 	socket.Connect("tcp://127.0.0.1:5000")
-	socket.Connect("tcp://127.0.0.1:6000")
 
 	for i := 0; i < 10; i++ {
 		msg := fmt.Sprintf("msg %d", i)
